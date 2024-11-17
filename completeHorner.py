f943b3771a6374e99a418225db7b301139bd41d4
Eyad Fahad <168282898+Commando-Xxt@users.noreply.github.com>
2024-11-17 17:48:16 +0300

    Update completeHorner.py

---
+++ completeHorner.py
@@ -0,0 +1,10 @@
+def compute_array(n, r, a):
+    # Ensure the array has n+1 elements
+    if len(a) != n + 1:
+        raise ValueError("Array 'a' must have n+1 elements.")
+
+    for k in range(n):
+        for j in range(n - 1, k - 1, -1):
+            a[j] = a[j] + r * a[j + 1]
+    
+    return a
